8aa765894f3a5f4d8a5e6b1911beac3d8e70c1c6
acarteas <mail@adam-carter.com>
2017-11-28 14:20:33 -0800

    -Added (readded) graph classes.

--- main.cpp
+++ main.cpp
@@ -3,32 +3,31 @@
 #include "sorting\Sorting.h"
 #include "benchmarks\SortingBenchmark.h"
 #include "ADTs\Vector.h"
+#include "ADTs\StringGraphNode.h"
 
 using namespace std;
 
 int main(void)
 {
-    Vector<int> v1{};
-    
-    SortingBenchmark bm{};
-    Vector<string> labels = { "Bubble", "Insertion", "Selection", "Merge", "Quick", "Shell" };
-    Vector<IndexedSort<int> *> sorters = { new BubbleSort<int>{},
-                                             new InsertionSort<int>{},
-                                             new SelectionSort<int>{},
-                                             new MergeSort<int>{},
-                                             new QuickSort<int>{},
-                                             new ShellSort<int>{}
-    };
-    for (int i = 3; i < labels.getSize(); i++)
-    {
-        cout << labels[i] << endl;
-        for (int j = 1000; j < 100000; j += 10000)
-        {
-            v1 = Vector<int>{};
-            cout << j;
-            cout << "\t" << bm.sortReverseSortedBenchmark(sorters[i], &v1, j) << endl;
-        }
-    }
-    
+    //maintains full collection of all vertices
+    unordered_map<string, StringGraphNode*> graph{};
+
+    //add vertices to graph
+    graph["A"] = new StringGraphNode{ "A" };
+    graph["B"] = new StringGraphNode{ "B" };
+    graph["C"] = new StringGraphNode{ "C" };
+    graph["D"] = new StringGraphNode{ "D" };
+    graph["E"] = new StringGraphNode{ "E" };
+    graph["F"] = new StringGraphNode{ "F" };
+
+    //A is connected to B and F
+    graph["A"]->addEdge(graph["B"]);
+    graph["A"]->addEdge(graph["F"]);
+
+    //B is connected to A, C, and E
+    graph["B"]->addEdge(graph["A"]);
+    graph["B"]->addEdge(graph["C"]);
+    graph["B"]->addEdge(graph["F"]);
+
     return 0;
 }
