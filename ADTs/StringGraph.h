fcf65537cc4025c6edaba53911566c15fa274fab
Benmoony <benmiller92@gmail.com>
2017-12-24 12:22:18 -0800

    used GraphNode class to build a print function for graph, as well as simple add edge and add vertices functions that go along with the buildGraphFunctions

--- ADTs/StringGraph.h
+++ ADTs/StringGraph.h
@@ -13,13 +13,12 @@ public:
 
 	void BulidGraph(vector<string> keys, unordered_map<string, vector<string>> edges) {
 		for (int i = 0; i < keys.size(); i++) {
-			string cur_key = keys.at(i);
-			graph[cur_key] = new StringGraphNode{ cur_key };
+			addVertice(keys.at(i));
 		}
 
 		for (auto key : edges) {
 			for (int i = 0; i < key.second.size(); i++) {
-				graph[key.first]->addEdge(graph[key.second.at(i)]);
+				addEdge(key.first, key.second.at(i));
 			}
 		}
 	}
@@ -28,7 +27,27 @@ public:
 		return graph;
 	}
 
+	void addEdge(string key, string value) {
+		graph[key]->addEdge[graph[value]];
+	}
 
+	void addVertice(string key) {
+		graph[key] = new StringGraphNode{ key };
+	}
+
+	void printGraph() {
+		for (auto vertice : graph) {
+			StringGraphNode* cur_node = vertice.second;
+			vector < GraphNode<string, string>*> cur_edge = cur_node->getEdges();
+			cout << vertice.first << " has edges: ";
+			for (int i = 0; i < cur_edge.size(); i++) {
+				cout << cur_edge.at(i)->getValue() << ", " ;
+			}
+			cout << endl;
+		}
+	}
+
+	
 };
 
 
--- main.cpp
+++ main.cpp
@@ -5,33 +5,13 @@
 #include "ADTs\Vector.h"
 #include "ADTs\StringGraphNode.h"
 #include "ADTs\StringGraph.h"
+#include "ADTs\GraphNode.h"
 
 using namespace std;
 
 int main(void)
 {
-    /*//maintains full collection of all vertices
-    unordered_map<string, StringGraphNode*> graph{};
-
-    //add vertices to graph
-    graph["A"] = new StringGraphNode{ "A" };
-    graph["B"] = new StringGraphNode{ "B" };
-    graph["C"] = new StringGraphNode{ "C" };
-    graph["D"] = new StringGraphNode{ "D" };
-    graph["E"] = new StringGraphNode{ "E" };
-    graph["F"] = new StringGraphNode{ "F" };
-
-    //A is connected to B and F
-    graph["A"]->addEdge(graph["B"]);
-    graph["A"]->addEdge(graph["F"]);
-
-    //B is connected to A, C, and E
-    graph["B"]->addEdge(graph["A"]);
-    graph["B"]->addEdge(graph["C"]);
-    graph["B"]->addEdge(graph["F"]);*/
-
 	unordered_map<string, vector<string>> edges{};
-	unordered_map<string, StringGraphNode*> cur_graph{};
 	vector<string> vertices;
 	StringGraph Test_Graph;
 
@@ -41,19 +21,8 @@ int main(void)
 
 	Test_Graph.BulidGraph(vertices, edges);
 
-	cur_graph = Test_Graph.getGraph();
-
-	for (auto vertice : cur_graph) {
-		StringGraphNode* cur_node = vertice.second;
-		cur_node->getEdges();
-		cur_node
-
-		cout << vertice.first << " has edges: ";
-	}
+	Test_Graph.printGraph();
 
 	system("Pause");
-
-	
-
     return 0;
 }
